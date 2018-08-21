44ac05c217bd17d58e87f1cb46ac7e88acd57625
Quinn Krug <quinn.krug@gmail.com>
2018-08-21 17:41:46 -0600

    Iteration 2

--- lib/round.rb
+++ lib/round.rb
@@ -1,3 +1,8 @@
+require 'pry'
+require './lib/deck.rb'
+require './lib/card.rb'
+require './lib/guess.rb'
+
 
 class Round
 
@@ -14,14 +19,28 @@ class Round
 
   def record_guess(guess)
     card_name = "#{guess[:value]} of #{guess[:suit]}"
+
     @guesses << Guess.new(card_name,current_card)
+
+    @deck = Deck.new(@deck.card_list.rotate)
+
 
   end
 
+  def number_correct
+    number_correct = 0
+    @guesses.each do |guess|
+      if guess.correct?
+        number_correct += 1
+      end
+    end
+    number_correct
+  end
 
+  def percent_correct
+    
+    ((number_correct/@guesses.length.to_f)*100).round(0)
 
+  end
 
 end
-
-
-#push.shift
--- test/round_test.rb
+++ test/round_test.rb
@@ -6,7 +6,7 @@ require './lib/card.rb'
 require './lib/round.rb'
 require './lib/guess.rb'
 
-class EventTest < Minitest::Test
+class RoundTest < Minitest::Test
   def test_it_exists
     card_1 = Card.new("3","Hearts")
     card_2 = Card.new("4", "Clubs")
@@ -33,12 +33,47 @@ class EventTest < Minitest::Test
   end
 
   def test_record_guess
-    skip
     card_1 = Card.new("3","Hearts")
     card_2 = Card.new("4", "Clubs")
+    guess = Guess.new(card_1,"3 of Hearts")
     deck = Deck.new([card_1, card_2])
     round = Round.new(deck)
-    assert_equal
+
+    round.record_guess({value:"3", suit: "Hearts"})
+
+    assert_equal "3", round.guesses.first.card.value
+    assert_equal "Hearts", round.guesses.first.card.suit
+    assert_equal "3 of Hearts", round.guesses.first.guess
+
+  end
+
+  def test_it_can_return_number_of_correct_guesses
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    guess = Guess.new(card_1,"3 of Hearts")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+
+    round.record_guess({value:"3", suit: "Hearts"})
+    round.record_guess({value:"2", suit:"Hearts"})
+    round.record_guess({value: "3", suit: "Hearts"})
+
+    assert_equal 2, round.number_correct
+
+  end
+
+  def test_it_can_calculate_percent_correct
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    guess = Guess.new(card_1,"3 of Hearts")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+
+    round.record_guess({value:"3", suit: "Hearts"})
+    round.record_guess({value:"2", suit:"Hearts"})
+    round.record_guess({value: "3", suit: "Hearts"})
+
+    assert_equal 67, round.percent_correct
   end
 
 
