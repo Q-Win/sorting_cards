8fea9995bdc2cdb03c9215f3f31af1e7b73cb8b3
Quinn Krug <quinn.krug@gmail.com>
2018-08-21 09:43:54 -0600

    fixed branch error

--- test/round_test.rb
+++ test/round_test.rb
@@ -15,6 +15,32 @@ class EventTest < Minitest::Test
     assert_instance_of Round, round
   end
 
+  def test_attributes_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal deck, round.deck
+    assert_equal [], round.guesses
+  end
+
+  def test_current_card
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal card_1, round.current_card
+  end
+
+  def test_record_guess
+    skip
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_equal
+  end
+
 
 
 end
